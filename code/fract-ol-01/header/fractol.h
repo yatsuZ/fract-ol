6c4bc62b486cdea196b0a5fe3f512925e88f5b89
yatsuZ <y.zaoui.pro@gmail.com>
2023-09-01 14:57:29 +0200

    prendre exemple fenetre_event .

--- code/fract-ol-01/header/fractol.h
+++ code/fract-ol-01/header/fractol.h
@@ -6,7 +6,7 @@
 /*   By: yatsu <yatsu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 14:13:02 by yatsu             #+#    #+#             */
-/*   Updated: 2023/09/01 01:35:53 by yatsu            ###   ########.fr       */
+/*   Updated: 2023/09/01 13:40:53 by yatsu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 // Toute les fonction que je compte utilise
 t_window	*ft_init_window(char *nom);
 t_window	*free_win(t_window *w);
+void		ft_add_event(t_window *w);
 
 #endif
--- code/fract-ol-01/src/add_event.c
+++ code/fract-ol-01/src/add_event.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   add_event.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yatsu <yatsu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/30 14:12:38 by yatsu             #+#    #+#             */
+/*   Created: 2023/09/01 13:39:25 by yatsu             #+#    #+#             */
 /*   Updated: 2023/09/01 13:39:51 by yatsu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/fractol.h"
-
-int	main(void)
-{
-	t_window	*window;
-
-	ft_printf("Hello World\n");
-	window = ft_init_window("Test");
-	if (!window)
-		return (0);
-	ft_add_event(window);
-	mlx_loop(window->mlx_ptr);
-	return (window = free_win(window), 0);
-}
